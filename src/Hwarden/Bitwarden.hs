cedc50829b351ad4feb0173902789871377f929b
Joonas Laukka <joonas.laukka@relexsolutions.com>
2026-05-15 01:06:04 +0300

    Remove stale Bitwarden imports

--- src/Hwarden/Bitwarden.hs
+++ src/Hwarden/Bitwarden.hs
@@ -10,22 +10,16 @@ module Hwarden.Bitwarden
   )
 where
 
-import Control.Exception (SomeException, try)
-import Data.Aeson (FromJSON (parseJSON), eitherDecodeStrict, withObject, (.:), (.:?))
-import qualified Data.ByteString.Char8 as BS8
+import Data.Aeson (FromJSON (parseJSON), withObject, (.:), (.:?))
 import Data.Maybe (mapMaybe)
 import Data.Text (Text)
-import Data.Bifunctor (first)
 import qualified Data.Text as T
 import Hwarden.Types
   ( ItemSummary (ItemSummary),
-    Password (Password),
-    SessionKey (SessionKey),
-    Username (Username)
+    Password,
+    SessionKey,
+    Username
   )
-import System.Exit (ExitCode (ExitFailure, ExitSuccess))
-import System.Environment (getEnvironment)
-import System.Process (CreateProcess (env), proc, readCreateProcessWithExitCode, readProcessWithExitCode)
 import Test.QuickCheck (Arbitrary (arbitrary), oneof)
 
 data UnlockError
@@ -55,45 +49,6 @@ instance Arbitrary ListItemsError where
 class Monad m => Bitwarden m where
   unlock :: Username -> Password -> m (Either UnlockError SessionKey)
   listItems :: SessionKey -> m (Either ListItemsError [ItemSummary])
-
-instance Bitwarden IO where
-  unlock (Username email) (Password password) = do
-    let args = [T.unpack email, T.unpack password, "--raw"]
-    result <-
-      try (readProcessWithExitCode "bw" ("login" : args) "") ::
-        IO (Either SomeException (ExitCode, String, String))
-    pure $
-      case result of
-        Left _ ->
-          Left UnlockUnavailable
-        Right (exitCode, stdoutText, stderrText) ->
-          case exitCode of
-            ExitSuccess ->
-              Right (SessionKey (T.strip (T.pack stdoutText)))
-            ExitFailure _ ->
-              Left (UnlockFailed (T.pack stderrText))
-  listItems (SessionKey rawSessionKey) = do
-    baseEnv <- getEnvironment
-    let command =
-          (proc "bw" ["list", "items"])
-            { env = Just (setEnvVar "BW_SESSION" (T.unpack rawSessionKey) baseEnv)
-            }
-    result <-
-      try (readCreateProcessWithExitCode command "") ::
-        IO (Either SomeException (ExitCode, String, String))
-    pure $
-      case result of
-        Left _ ->
-          Left ListItemsUnavailable
-        Right (exitCode, stdoutText, stderrText) ->
-          case exitCode of
-            ExitSuccess -> do
-              bwItems <- 
-                first (ListItemsFailed . T.pack) $ 
-                  eitherDecodeStrict (BS8.pack stdoutText)
-              return $ extractLoginItems bwItems
-            ExitFailure _ ->
-              Left (ListItemsFailed (T.pack stderrText))
 
 data BwItem = BwItem
   { bwItemId :: Text,
@@ -129,6 +84,3 @@ toItemSummary item =
             (bwItemName item)
             (bwLoginUsername login)
         )
-
-setEnvVar :: String -> String -> [(String, String)] -> [(String, String)]
-setEnvVar key value envVars = (key, value) : filter ((/= key) . fst) envVars
