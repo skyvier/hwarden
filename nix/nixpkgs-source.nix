a9b5946f43e1f62e173f34d7c59b5ba06991b59a
Joonas Laukka <joonas.laukka@iki.fi>
2026-06-04 16:40:39 +0300

    Clean up nixpkg pinning for release

--- nix/nixpkgs-source.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/687f05a9184cad4eaf905c48b63649e3a86f5433.tar.gz";
-  sha256 = "1n1lqgnk84mf28v23f3q8z6xwc79biqwqqy84cg75mrrpa65k4mx";
-}
